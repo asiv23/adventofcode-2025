09b7a68eb521b86d7d18b9ef5abd0d9e74bf5838
anna.si <anna.si@inet.co.th>
2025-12-15 17:18:35 +0700

    add day 8 solution

--- day8.py
+++ day8.py
@@ -25,31 +25,63 @@ testData = '''162,817,812
 def dist(n, m):
 	return(((n[0] - m[0]) ** 2 + (n[1] - m[1]) ** 2 + (n[2] - m[2]) ** 2) ** 0.5)
 
-# boxes = [[int(i) for i in x.split(",")] for x in data.split('\n')]
-boxes = [[int(i) for i in x.split(",")] for x in testData.split('\n')]
+def findDist(boxes):
+	distances = []
+	for i in range(0, len(boxes)):
+		for j in range(i+1, len(boxes)):
+			distances.append(tuple([dist(boxes[i], boxes[j]), i, j]))
+	distances.sort(key=lambda x: x[0])
+	return(distances)
 
-distances = []
-for i in range(0, len(boxes)):
-	for j in range(i+1, len(boxes)):
-		distances.append([dist(boxes[i], boxes[j]), i, j])
-distances.sort(key=lambda x: x[0])
-# distances = distances[0:10]
-# print(distances)
+def findDupes(graph):
+	dupes = True
+	while dupes:
+		dupes = False
+		for i in range(len(graph)):
+			for j in range(i+1, len(graph)):
+				# print(graph)
+				if len(graph[i].intersection(graph[j])) > 0:
+					graph[i].update(graph[j])
+					del graph[j]
+					dupes = True
+					break
+	return(graph)
 
+def part1(distances):
+	graph = [set(distances[0][1:])]
+	for i in range(1, 1000):
+		found = False
+		for j in range(0, len(graph)):
+			if len(graph[j].intersection(distances[i])) > 0:
+				graph[j].add(distances[i][1])
+				graph[j].add(distances[i][2])
+				found = True
+				break
+		if not found:
+			graph.append(set(distances[i][1:]))
 
-graph = [distances[0][1:]]
-for i in distances[1:11]:
-	# print("test", graph)
-	contains = False
+	graph = (findDupes(graph))
+	graph.sort(key=len, reverse=True)
+	print(len(graph[0])*len(graph[1])*len(graph[2])) 
+
+input = [[int(i) for i in x.split(",")] for x in data.split('\n')]
+distances = findDist(input)
+# part1(distances)
+
+graph = [set(distances[0][1:])]
+for i in range(1, len(distances)):
+	found = False
 	for j in range(0, len(graph)):
-		if i[1] in graph[j] and i[2] not in graph[j]:
-			graph[j].append(i[2])
-			contains = True
-		elif i[2] in graph[j] and [1] not in graph[j]:
-			graph[j].append(i[1])
-			contains = True
-	if contains == False:
-		graph.append(i[1:])
-
-graph.sort(key=len, reverse=True)
-[print(x) for x in graph]
+		if len(graph[j].intersection(distances[i])) > 0:
+		# if distances[i][1] in graph[j] or distances[i][2] in graph[j]:
+			graph[j].add(distances[i][1])
+			graph[j].add(distances[i][2])
+			found = True
+			break
+	if not found:
+		graph.append(set(distances[i][1:]))
+	graph = findDupes(graph)
+	if len(graph[0]) == len(input):
+		y = (list(distances[i]))
+		print(input[y[1]][0] * input[y[2]][0])
+		break
